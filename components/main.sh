ea589380883831f3e5850fde3b4eeed5ff647596
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-05 10:38:19 +0530

    update 3

--- components/main.sh
+++ components/main.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 COMPONENT=$1
-if [ ! -f components/${COMPONENT}.sh ] ; then
+if [ ! -f components/"${COMPONENT}".sh ] ; then
   echo "Invalid Component name Is Provided"
   exit 1
 fi
 
-bash components/${COMPONENT}.sh
+bash components/"${COMPONENT}".sh
 
