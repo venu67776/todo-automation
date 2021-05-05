f7b4433ef5fd6009a107eec2b07f94037ac7ea01
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-05 15:53:17 +0530

    2

---
+++ components/common.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+Head() {
+  echo -e "\e[1;36m ============================ $1  \e[0m"
+  echo -e "\e[1;36m ============================ $1  \e[0m" >>$LOG
+}
+
+Stat() {
+  if [ "$1" -eq 0 ]; then
+    echo -e "\e[1;32m SUCCESS\e[0m"
+  else
+    echo -e "\e[1;31m FAILURE\e[0m"
+    echo -e "\n\e[1;33m You can refer log file fore more information, Log file Path = ${LOG}\e[0m"
+    exit 1
+  fi
+}
+
+OS_PREREQ() {
+  set-hostname ${COMPONENT}
+  Head "Updating APT Repos"
+  apt update &>>$LOG
+  Stat $?
+}
+
+ERROR() {
+  echo -e "\e[1;31m$1\e[0m"
+}
+
--- components/frontend.sh
+++ components/frontend.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
- echo hello
+#!/bin/bash
+
+source components/common.sh
+
+DOMAIN="zsdevops01.online"
+
+OS_PREREQ
+
+Head "Installing Nginx"
+apt install nginx -y &>>$LOG
+Stat $?
+
+DOWNLOAD_COMPONENT
+
+Head "Remove Default Configuration"
+rm -rf /var/www/html /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
+Stat $?
+
+Head "Unzip Downloaded Archive"
+cd /var/www && unzip -o /tmp/frontend.zip &>>$LOG && mv frontend-main/* . && mv static html && rm -rf frontend-main README.md
+Stat $?
+
+Head "Update Nginx Configuration"
+mv roboshop.conf /etc/nginx/sites-enabled/roboshop.conf
+for comp in catalogue cart user shipping payment ; do
+  sed -i -e "/$comp/ s/localhost/${comp}.zsdevops01.online/" /etc/nginx/sites-enabled/roboshop.conf
+done
+Stat $?
+
+Head "Restart Nginx Service"
+systemctl restart nginx
+Stat $?
+
