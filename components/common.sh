b3e6f7343d0520d809a338528a10f5679b2a6bbc
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-06 15:45:21 +0530

    new

--- components/common.sh
+++ components/common.sh
@@ -28,6 +28,6 @@ ERROR() {
 
 DOWNLOAD_COMPONENT() {
   Head "Downloading ${COMPONENT} Component"
-  git clone https://github.com/venu67776/${COMPONENT}.git
+  git clone https://github.com/venu67776/${COMPONENT}.git &>>$log
   Stat $?
 }
