9a3fb2d6b7cf56346eaf71029f9f61e2041e068f
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-07 22:07:41 +0530

    new

--- components/todo.sh
+++ components/todo.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
-echo venu
-echo gopal
-echo raja
-echo riyansh
-echo ramu
+
+source components/common.sh
+
+OS_PREREQ
+
+
+Head "Installing npm"
+apt install npm -y &>>$LOG
+Stat $?
+
+
+DOWNLOAD_COMPONENT
+
+Head "Extract Downloaded Archive"
+cd /home/ubuntu && rm -rf todo && unzip -o /tmp/todo.zip &>>$LOG && mv todo-main todo  && cd todo && npm install &>>$LOG
+Stat $?
+
+Head "Update EndPoints in Service File"
+sed -i -e "s/user_endpoint/login.venu6776.tk/" /home/ubuntu/todo/systemd.service
+Stat $?
+
+
+Head "Setup SystemD Service"
+mv /home/ubuntu/todo/systemd.service /etc/systemd/system/todo.service && systemctl daemon-reload && systemctl start todo && systemctl enable todo &>>$LOG
+Stat $?
