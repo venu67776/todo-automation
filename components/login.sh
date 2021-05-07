f19977b95b604332db509c5298769a117079f4f5
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-07 11:53:09 +0530

    new

--- components/login.sh
+++ components/login.sh
@@ -4,4 +4,16 @@ source components/common.sh
 
 OS_PREREQ
 
+Head "Installing Golang"
+apt install golang -y &>>$LOG
+Stat $?
 
+DOWNLOAD_COMPONENT
+
+Head "Update Login "
+cd Login
+export GOPATH=/go
+go get
+go build
+./Login
+Stat $?
