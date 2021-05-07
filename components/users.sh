4f87378fe7d7a8768970619962482cc09c4a4e24
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-07 21:07:12 +0530

    new

---
+++ components/users.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source components/common.sh
+
+OS_PREREQ
+
+Head "Installing java"
+sudo apt install openjdk-8-jdk &>>$LOG
+Stat $?
+
+Head "Installing maven"
+apt install maven -y &>>$LOG
+Stat $?
+
+
+DOWNLOAD_COMPONENT
+
+Head "Extract Downloaded Archive"
+cd /home/ubuntu && rm -rf users && unzip -o /tmp/users.zip &>>$LOG && mv users-main users  && cd users && mvn clean package &>>$LOG 
+Stat $?
+
