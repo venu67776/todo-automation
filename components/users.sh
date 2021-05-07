c806f6aa3ab3e8f8284684bf1e5d9b4238a6ce49
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-07 21:24:00 +0530

    new

--- components/users.sh
+++ components/users.sh
@@ -19,3 +19,12 @@ Head "Extract Downloaded Archive"
 cd /home/ubuntu && rm -rf users && unzip -o /tmp/users.zip &>>$LOG && mv users-main users  && cd users && mvn clean package &>>$LOG 
 Stat $?
 
+Head "Update EndPoints in Service File"
+sed -i -e "s/user_endpoint/login.venu6776.tk/" /home/ubuntu/login/systemd.service
+Stat $?
+
+
+Head "Setup SystemD Service"
+mv /home/ubuntu/users/systemd.service /etc/systemd/system/users.service && systemctl daemon-reload && systemctl start users && systemctl enable users &>>$LOG
+Stat $?
+
