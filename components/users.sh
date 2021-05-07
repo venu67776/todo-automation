acce49102709fc8f0be922f497a935bbeeeeb89b
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-07 21:45:09 +0530

    new

--- components/users.sh
+++ components/users.sh
@@ -16,7 +16,7 @@ Stat $?
 DOWNLOAD_COMPONENT
 
 Head "Extract Downloaded Archive"
-cd /home/ubuntu && rm -rf users && unzip -o /tmp/users.zip &>>$LOG && mv users-main users  && cd users && mvn clean package &>>$LOG 
+cd /home/ubuntu && rm -rf users && unzip -o /tmp/users.zip &>>$LOG && mv users-main users  && cd users && mvn clean package &>>$LOG && mv target/users-api-0.0.1.jar users.jar
 Stat $?
 
 Head "Update EndPoints in Service File"
