0c04397837340695fbd90c395af2058235be5bd3
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-07 14:05:52 +0530

    new

--- components/user.sh
+++ components/user.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-echo helloworld
+
+source components/common.sh
+
+OS_PREREQ
+
+Head "Installing Golang"
+apt install golang -y &>>$LOG
+Stat $?
+
+DOWNLOAD_COMPONENT
+
+Head "Extract Downloaded Archive"
+cd /home/ubuntu && rm -rf login && unzip -o /tmp/login.zip &>>$LOG && mv login-main login && cd /home/ubuntu/login && export GOPATH=/go && go build
+Stat $?
+
+Head "Update EndPoints in Service File"
+sed -i -e "s/user_endpoint/login.venu6776.tk/" /home/ubuntu/login/systemd.service
+Stat $?
+
+
+//Head "Setup SystemD Service"
+//mv /home/ubuntu/login/systemd.service /etc/systemd/system/login.service && systemctl daemon-reload && systemctl start login && systemctl enable login &>>$LOG
+//Stat $?
