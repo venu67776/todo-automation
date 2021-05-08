3d937e9779a5132a55f68a400552712ca876088b
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-08 10:29:21 +0530

    new

--- components/frontend.sh
+++ components/frontend.sh
@@ -16,20 +16,11 @@ Stat $?
 
 DOWNLOAD_COMPONENT
 
-Head "Remove Default Configuration"
-rm -rf /var/www/html /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
-Stat $?
-
 Head "Unzip Downloaded Archive"
-cd /var/www && unzip -o /tmp/frontend.zip &>>$LOG && mv frontend-main/* . && mv static html && rm -rf frontend-main README.md
+cd /var/www && unzip -o /tmp/frontend.zip &>>$LOG && rm -rf frontend.zip && mv frontend-main frontend && cd frontend
 Stat $?
 
-Head "Update Nginx Configuration"
-mv todp.conf /etc/nginx/sites-enabled/todo.conf
-for comp in login todo ; do
-  sed -i -e "/$comp/ s/localhost/${comp}.venu6776.tk/" /etc/nginx/sites-enabled/todo.conf
-done
-Stat $?
+
 
 Head "Restart Nginx Service"
 systemctl restart nginx
