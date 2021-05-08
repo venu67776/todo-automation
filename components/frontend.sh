23e88a01cfac239cbd6521fa04dd956da249003c
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-08 15:40:42 +0530

    new

--- components/frontend.sh
+++ components/frontend.sh
@@ -10,6 +10,10 @@ Head "Installing Nginx"
 apt install nginx -y &>>$LOG
 Stat $?
 
+Head "Installing Npm"
+apt install npm -y &>>$LOG
+Stat $?
+
 DOWNLOAD_COMPONENT
 
 Head "Unzip Downloaded Archive"
@@ -20,10 +24,6 @@ Head "Update Nginx Configuration"
 cd /etc/nginx/sites-enabled
 sed -i 's|/var/www/html|/var/www/html/frontend/dist|g' /etc/nginx/sites-enabled/default
 Stat $?
-
-Head "Installing Npm"
- apt install npm -y
- Stat $?
 
 Head "update Frontend Configuration"
 cd /var/www/html/frontend && npm install  && npm run build && npm start &>>$LOG
