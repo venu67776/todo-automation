e1e71d9c7c6e3abce191ad714f888958c4812929
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-09 22:52:19 +0530

    new

--- components/frontend.sh
+++ components/frontend.sh
@@ -30,7 +30,7 @@ Head "update frontend configuration"
 cd /var/www/html/vue/frontend  && sudo npm install --unsafe-perm sass sass-loader node-sass wepy-compiler-sass && npm run build && npm start
  
 Head "update end points in service file"
-cd /var/www/html/vue/frontend/config
+cd /var/www/html/vue/frontend/
 export AUTH_API_ADDRESS=http://login.venu6766.tk:8080
 export TODOS_API_ADDRESS=http://todos.venu6766.tk:8080
 
