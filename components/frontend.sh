df18861878af39ae8c0bb11987fd7634958d258f
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-09 22:59:36 +0530

    new

--- components/frontend.sh
+++ components/frontend.sh
@@ -28,11 +28,13 @@ Stat $?
 
 Head "update frontend configuration"
 cd /var/www/html/vue/frontend  && sudo npm install --unsafe-perm sass sass-loader node-sass wepy-compiler-sass &>>$LOG && npm run build &>>$LOG && npm start &>>$LOG
+ Stat $?
  
 Head "update end points in service file"
 cd /var/www/html/vue/frontend
 export AUTH_API_ADDRESS=http://login.venu6766.tk:8080
 export TODOS_API_ADDRESS=http://todos.venu6766.tk:8080
+Stat $?
 
 Head "Restart the Nginx service"
  systemctl restart nginx
