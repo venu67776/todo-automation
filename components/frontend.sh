af14ed23ad2ea5fec96a08ad4aed6f9d4ee91009
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-09 20:52:51 +0530

    venu

--- components/frontend.sh
+++ components/frontend.sh
@@ -18,7 +18,7 @@ Stat $?
 DOWNLOAD_COMPONENT
 
 Head "Unzip Downloaded Archive"
-cd /var/www/html && mkdir vue && cd vue && unzip -o /tmp/frontend.zip &>>$LOG && rm -rf frontend.zip  && rm -rf frontend && mv frontend-main frontend && cd frontend
+cd /var/www/html &&rm -rf vue && mkdir vue && cd vue && unzip -o /tmp/frontend.zip &>>$LOG && rm -rf frontend.zip  && rm -rf frontend && mv frontend-main frontend && cd frontend
 Stat $?
 
 Head "Update Nginx Configuration"
