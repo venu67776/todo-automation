2d2831a0fad02eb3262aaf08592b55d3a9de0dcc
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-09 21:58:30 +0530

    new

--- components/frontend.sh
+++ components/frontend.sh
@@ -14,16 +14,21 @@ Head "Install nodejs and npm"
 apt install npm -y
 Stat $?
 
+Head "Update Nginx Configuration"
+cd /etc/nginx/sites-enabled
+sed -i 's|/var/www/html|/var/www/html/vue/frontend/dist|g' /etc/nginx/sites-enabled/default
+Stat $?
+
 DOWNLOAD_COMPONENT
 
 Head "Unzip Downloaded Archive"
 cd /var/www/html &&rm -rf vue && mkdir vue && cd vue && unzip -o /tmp/frontend.zip &>>$LOG && rm -rf frontend.zip  && rm -rf frontend && mv frontend-main frontend && cd frontend
 Stat $?
 
-Head "Update Nginx Configuration"
-cd /etc/nginx/sites-enabled
-sed -i 's|/var/www/html|/var/www/html/vue/frontend/dist|g' /etc/nginx/sites-enabled/default
-Stat $?
-
 Head "update frontend configuration"
 cd /var/www/html/vue/frontend  && sudo npm install --unsafe-perm sass sass-loader node-sass wepy-compiler-sass && npm run build && npm start
+
+
+Head "Restart the Nginx service"
+ systemctl restart nginx
+ Stat $?
