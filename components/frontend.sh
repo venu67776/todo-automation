1492f1b86fa8286d1581d56078b42b1d75727959
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-09 20:39:27 +0530

    new

--- components/frontend.sh
+++ components/frontend.sh
@@ -10,21 +10,21 @@ Head "Installing Nginx"
 apt install nginx -y &>>$LOG
 Stat $?
 
-DOWNLOAD_COMPONENT
-
-Head "Unzip Downloaded Archive"
-cd /var/www/html && unzip -o /tmp/frontend.zip &>>$LOG && rm -rf frontend.zip  && rm -rf frontend && mv frontend-main frontend && cd frontend
-Stat $?
-
-Head "Update Nginx Configuration"
-cd /etc/nginx/sites-enabled
-sed -i 's|/var/www/html|/var/www/html/frontend/dist|g' /etc/nginx/sites-enabled/default
-Stat $?
-
 Head "Install nodejs and npm"
 apt install nodejs -y
 apt install npm -y
 Stat $?
 
+DOWNLOAD_COMPONENT
+
+Head "Unzip Downloaded Archive"
+cd /var/www/html && mkdir vue && cd vue && unzip -o /tmp/frontend.zip &>>$LOG && rm -rf frontend.zip  && rm -rf frontend && mv frontend-main frontend && cd frontend
+Stat $?
+
+Head "Update Nginx Configuration"
+cd /etc/nginx/sites-enabled
+sed -i 's|/var/www/html|/var/www/html/vue/frontend/dist|g' /etc/nginx/sites-enabled/default
+Stat $?
+
 Head "update frontend configuration"
-cd /var/www/html/frontend  && npm clean cache --force &&npm install
+cd /var/www/html/frontend  && npm install && npm run build && npm start
