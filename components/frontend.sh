234ab1a45cbacdab601509c24e9c59480105790e
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-05-11 11:11:34 +0530

    new

--- components/frontend.sh
+++ components/frontend.sh
@@ -17,7 +17,7 @@ Stat $?
 
 Head "Update Nginx Configuration"
 cd /etc/nginx/sites-enabled
-sed -i 's|/var/www/html/vue/frontend/dist|g' /etc/nginx/sites-enabled/default 
+sed -i 's|var/www/html/vue/frontend/dist|g' /etc/nginx/sites-enabled/default 
 if [ $? -ne 0 ]; then
 sed -i 's|/var/www/html|/var/www/html/vue/frontend/dist|g' /etc/nginx/sites-enabled/default
 Stat $?
