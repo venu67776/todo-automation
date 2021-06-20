571883f7ffbeb5766c5dd734da86104256e3baac
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-06-20 13:37:37 +0530

    new

--- components/frontend.sh
+++ components/frontend.sh
@@ -22,7 +22,7 @@ Stat $?
 DOWNLOAD_COMPONENT
 
 Head "Unzip Downloaded Archive"
-cd /var/www/html &&rm -rf vue && mkdir vue && cd vue && unzip -o /tmp/frontend.zip &>>$LOG && rm -rf frontend.zip  && rm -rf frontend && mv frontend-main frontend && cd frontend
+cd /var/www/html && unzip -o /tmp/frontend.zip &>>$LOG && rm -rf frontend.zip  && rm -rf frontend && mv frontend-main frontend && cd frontend
 Stat $?
 
 Head "update frontend configuration"
