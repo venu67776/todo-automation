4d872301121aa5b2cca3177cd318e5bb25df7dad
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-07-19 16:02:42 +0530

    new

--- 1.sh
+++ 1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sshpass -p 'DevOps321' ssh ubuntu@login.venu6766.tk  "git clone http://github.com/venu67776.todo-automation.git ; cd todo-automation ; sudo bash todoapp.sh login"
+sshpass -p 'DevOps321' ssh ubuntu@login.venu6766.tk  "https://github.com/venu67776/todo-automation.git; cd todo-automation ; sudo bash todoapp.sh login"
