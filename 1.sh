d169267ebc8bf67cb919083f4f3bfb11f17efb73
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-07-19 16:21:33 +0530

    new

--- 1.sh
+++ 1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sshpass -p 'DevOps321' ssh ubuntu@login.venu6766.tk  "cd /root && sudo git clone https://github.com/venu67776/todo-automation.git""
+sshpass -p 'DevOps321' ssh ubuntu@login.venu6766.tk  "cd /root && sudo git clone https://github.com/venu67776/todo-automation.git"
