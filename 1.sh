1578d1aa98143fe37d67c9406c967c597ee4c464
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-07-20 10:11:31 +0530

    new

--- 1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sshpass -p 'DevOps321' ssh ubuntu@redis.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh redis"
-sshpass -p 'DevOps321' ssh ubuntu@todo.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh todo"
-sshpass -p 'DevOps321' ssh ubuntu@users.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh users"
-sshpass -p 'DevOps321' ssh ubuntu@login.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh login"
-sshpass -p 'DevOps321' ssh ubuntu@frontend.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh frontend"
