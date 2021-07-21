b0cea1f5174ff5d9500bda8ea33b47fc6d1d9f1e
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-07-21 12:36:16 +0530

    new

--- 1.sh
+++ 1.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#! /bin/bash
 
-sshpass -p 'DevOps321' ssh ubuntu@redis.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh redis"
-sshpass -p 'DevOps321' ssh ubuntu@todo.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh todo"
-sshpass -p 'DevOps321' ssh ubuntu@users.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh users"
-sshpass -p 'DevOps321' ssh ubuntu@login.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh login"
-sshpass -p 'DevOps321' ssh ubuntu@frontend.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh frontend"
+for component in redis todo users login frontend ; do
+
+   sshpass -p DevOps321 ssh ubuntu@$component.venu6766.uk "git clone https://github.com/venu67776/Todo-automation.git ; cd Todo-automation ; sudo bash Todo.sh $component"
+
+done
