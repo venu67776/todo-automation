32469be1eed2be5d43baa9f366db8867b12c5fa7
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-07-21 12:54:40 +0530

    new

--- 1.sh
+++ 1.sh
@@ -2,6 +2,6 @@
 
 for component in redis todo users login frontend ; do
 
-   sshpass -p DevOps321 ssh ubuntu@${component}.venu6766.tk "git clone https://github.com/venu67776/Todo-automation.git ; cd todo-automation ; sudo bash todoapp.sh ${component}"
+   sshpass -p DevOps321 ssh ubuntu@${component}.venu6766.tk "git clone https://github.com/venu67776/todo-automation.git ; cd todo-automation ; sudo bash todoapp.sh ${component}"
 
 done
