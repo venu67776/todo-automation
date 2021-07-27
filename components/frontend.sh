e1f41fddfae8b600aac8502f199a156c71f9918a
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-07-27 15:03:21 +0530

    new

--- components/frontend.sh
+++ components/frontend.sh
@@ -17,6 +17,10 @@ Stat $?
 
 DOWNLOAD_COMPONENT
 
+Head "Remove Default Configuration"
+rm -rf /var/www/html /etc/nginx/sites-enabled/default /etc/nginx/sites-available/default
+Stat $?
+
 Head "Unzip Downloaded Archive"
 cd /var/www/html && unzip -o /tmp/frontend.zip &>>$LOG && rm -rf frontend.zip  && rm -rf frontend && mv frontend-main frontend && cd frontend
 Stat $?
