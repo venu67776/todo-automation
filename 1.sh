ac1affd0aa26261d163afb7628ffaff921cd9d84
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-07-27 12:33:01 +0530

    new

--- 1.sh
+++ 1.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+for comp in redis,todo,users,login.frontend
 
-sshpass -p 'DevOps321' ssh ubuntu@redis.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh redis"
-sshpass -p 'DevOps321' ssh ubuntu@todo.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh todo"
-sshpass -p 'DevOps321' ssh ubuntu@users.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh users"
-sshpass -p 'DevOps321' ssh ubuntu@login.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh login"
-sshpass -p 'DevOps321' ssh ubuntu@frontend.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh frontend"
+sshpass -p 'DevOps321' ssh ubuntu@$comp.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh $comp"
+
+done
