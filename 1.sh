03e8823cd2e66bbe66775960670be8c61140ea66
venu67776 <83317608+venu67776@users.noreply.github.com>
2021-07-28 10:54:59 +0530

    new

---
+++ 1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for comp in redis todo users login frontend ; do
+
+sshpass -p 'DevOps321' ssh ubuntu@$comp.venu6766.tk  "sudo git clone https://github.com/venu67776/todo-automation.git && cd todo-automation && sudo bash todoapp.sh $comp"
+done
